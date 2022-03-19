8a25086e27bc0c8ae3492df9b24ef5449d2f14b7
Анастасия Меньшикова <menschickova2011@mail.ru>
2022-03-19 19:30:40 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_14
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Cat сat = new Cat("Кошка", "Мяу");
+            Console.WriteLine(сat.Print());
+            Dog dog = new Dog("Собака", "Гав");
+            Console.WriteLine(dog.Print());
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_14
+{
+    
+     abstract class Animal
+    {
+       public abstract string Name{ get; set; }
+         
+        public  Animal (string name)
+        {
+            Name=name;
+        }
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+       
+    }
+    class Cat:Animal
+    {
+        public string say;
+        public string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public Cat(string name, string say)
+            :base(name)
+        {
+            this.say = say;
+            
+        }
+        public override void Say()
+        {
+            Console.WriteLine(say);
+            
+        }
+        public string Print()
+        {
+            return $" Животное - {name}, говорит - {say}";
+        }
+    }
+    class Dog : Animal
+    {
+        public string say;
+        public string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public Dog(string name, string say)
+            : base(name)
+        {
+            this.say = say;
+
+        }
+        public override void Say()
+        {
+            Console.WriteLine(say);
+            
+
+        }
+        public  string Print()
+        {
+            return $" Животное - {name}, говорит - {say}";
+        }
+    }
+   
+}
+
